cca097bfad22bbe1245eb121d1a44eca7bbbf6e7
dickinsonjl <dickinsonjl@users.noreply.github.com>
2016-02-10 12:34:55 +0000

    adding relative path info per file
    Used some code from project: https://github.com/bpicolo/CopyRelativePath

--- listopenfiles.py
+++ listopenfiles.py
@@ -1,4 +1,5 @@
 import sublime,sublime_plugin,os
+from os.path import relpath
 
 class ListOpenFilesCommand(sublime_plugin.TextCommand):
 
@@ -9,6 +10,23 @@ class ListOpenFilesCommand(sublime_plugin.TextCommand):
         fileNames = ''
         for view in views:
             if view and view.file_name():
-                fileNames += os.path.abspath(view.file_name())+'\n'
+                # fileNames += os.path.basename(view.file_name())+'\n'
+                fileNames += self.getRelativePathOfFile(view.file_name())+'\n'
 
         window.new_file().insert(edit, 0, "List of open files:\n\n"+fileNames)
+
+    def getRelativePathOfFile(self, filename):
+        if len(filename) > 0:
+            try :
+                thePath = (
+                    min(
+                        (
+                            relpath(filename, folder).replace("\\", "/")
+                            for folder in sublime.active_window().folders()
+                        ),
+                        key=len,
+                    )
+                )
+            except :
+                thePath = filename
+            return thePath
